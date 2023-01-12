95d55a2d06367534a4a78d292ed8985fb90a3156
Rishikesh Naware <rishikeshnaware7@gmail.com>
2023-01-12 13:20:49 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const btn = document.getElementById("myBtn");
+const degrees = document.getElementById("input");
+const state = document.getElementById("inputState");
+let result = document.getElementById("resultmsg");
+let span = document.getElementById("empty");  
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  console.log(state.value);
+  if (!degrees.value) {
+    span.innerHTML = "Enter value for conversion";
+    setTimeout(()=>{span.innerHTML = "";}, 2000);
+  } else if (state.value == "fahrenheit" && degrees.value != "") {
+    result.innerHTML = (parseFloat(degrees.value) * 9) / 5 + 32 + "°F";
+    setTimeout(()=>{result.innerHTML = "";}, 5000);
+  } else if (state.value == "celsius" && degrees.value != "") {
+    result.innerHTML = parseFloat(degrees.value) + "°C";
+    setTimeout(()=>{result.innerHTML = "";}, 5000);
+  } else if (state.value == "kelvin" && degrees.value != "") {
+    result.innerHTML = parseFloat(degrees.value) + 273.15 + " K";
+    setTimeout(()=>{result.innerHTML = "";}, 5000);
+  }
+});
